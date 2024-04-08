82deee2564b5716e2385b85198546b1bb7b65cf5
Minhajul-alam <alam.minhajul@outlook.com>
2024-04-08 19:19:06 +0600

    Added banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,7 +1,13 @@
 const Banner = () => {
   return (
-    <div>
-      <h1>banner</h1>
+    <div className="max-w-7xl mx-auto text-center py-16 space-y-5">
+      <h1 className="text-4xl">
+        Build your next App <br /> even faster!
+      </h1>
+      <p className="text-[#989898]">
+        Beautifully designed, expertly crafted Flutter components and templates,
+        to jumpstart <br /> your projects and speed up your development process.
+      </p>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../../components/banner/Banner";
+
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <Banner />
     </div>
   );
 };
